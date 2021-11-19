9508974a871bb6e26539de039392cd073a00daf8
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2021-11-19 15:22:27 +0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: "/2021",
+  siteMetadata: {
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-emilia-core/gatsby-config.js
+    siteTitle: `2021`,
+    siteTitleAlt: `2021 | 📷 of Long Nhat Nguyen`,
+    siteHeadline: `2021 | 📷 of Long Nhat Nguyen`,
+    siteUrl: `https://retro90s.github.io/2021/`,
+    siteDescription: "📷 of Long Nhat Nguyen in 2021",
+    siteLanguage: `en`,
+    author: `@torn4dom4n`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `2021`,
+        socialMedia: [{}],
+        location: `Vietnam`,
+      },
+    },
+  ],
+}
