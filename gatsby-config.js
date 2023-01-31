d570076f032dd7bf9506a1c36e1ad3286ea01ba6
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-02-01 00:03:39 +0700

    ready to archive

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,50 @@ module.exports = {
     siteTitle: `2021`,
     siteTitleAlt: `2021 | 📷 by Long Nhat Nguyen`,
     siteHeadline: `2021 | 📷 by Long Nhat Nguyen`,
-    siteUrl: `https://retro90s.github.io/2021/`,
+    siteUrl: `https://microlens.github.io/2021`,
     siteDescription: "Photos by Long Nhat Nguyen",
-    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
     author: `@torn4dom4n`,
   },
+  trailingSlash: `never`,
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
       options: {
         name: `2021`,
         socialMedia: [{}],
+        showThemeAuthor: false,
         location: `Vietnam`,
       },
     },
-  ],
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `2021 | 📷 by Long Nhat Nguyen`,
+        short_name: `2021`,
+        description: `Photos by Long Nhat Nguyen`,
+        start_url: `/`,
+        background_color: `#fff`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ].filter(Boolean),
 }
